7647c43b2593a6df4378285fdf1523ec704781c4
Chibadoii <ghostrider.max@mail.ru>
2024-02-01 01:34:26 +0300

    last

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
-use std::io;
 use std::io::stdin;
-use std::task::Context;
 use sha2::{Digest, Sha256};
 use sha2::digest::{DynDigest, Output, Update};
-use sha2::digest::core_api::CoreWrapper;
+
 
 fn main() {
    let (zero, num_quentity) = input();
     let mut str_hash_num = String::new();
     let mut num: i64  = 0;
     let mut quantity = 0;
+
+
     loop{
         num +=1;
         str_hash_num = num.to_string();
         let res_hash = calc_hesh(str_hash_num);
-        println!("{}", &res_hash);
+        //println!("{}", &res_hash);
 
         let (num_zero) =iter_char_hash((&res_hash).to_string(),zero);
 
         if num_zero {
-            println!("{} ", num);
-            println!("{}", res_hash);
+            println!("{}, ", &num);
+            println!("{}", &res_hash);
             quantity +=1;
         }
         if quantity == num_quentity {
@@ -51,7 +51,7 @@ fn calc_hesh (num: String) -> String
 {
     let hash = Sha256::new().chain(num).finalize();
     let s = format!("{:x}", hash);
-    println!("{}", &s);
+    //println!("{}", &s);
 
     s
 }
@@ -61,16 +61,18 @@ fn iter_char_hash (res_hash: String, zero: i64) -> bool {
     let mut num_zero = 0;
     let char_zero = '0';
 
+    'oo: while num_zero <= zero {
         for c in res_hash.chars().rev() {
             if c == char_zero {
                 num_zero += 1;
-            } else { break; }
-
+            } else { break 'oo; }
+        }
     }
+
     if num_zero == zero{
         res_num = true;
     }
-
+    num_zero=0;
     //вывод счетчика для завершения луп
     res_num
 }
